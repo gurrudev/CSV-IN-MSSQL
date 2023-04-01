332f2f81e02eea6d021210e65c4ceda0f4f95751
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-04-01 14:58:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Upload Image in MongoDB
+
+
+📦Install
+
+```sh
+npm install
+```
+
+
+💻Usage
+ 
+```js
+// replace the value in your .env 
+const config = {
+    user: 'xxxx',
+    password: 'xxxx',
+    server: 'xxxx', // e.g. 'localhost'
+    database: 'xxxx',
+    options: {
+        encrypt: true, // For secure connection
+        trustServerCertificate: true,
+    }
+};
+```
+
+
+🚀Start
+
+```sh
+npm start
+```
---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const csvParser = require('csv-parser');
+const fs = require('fs');
+const sql = require('mssql');
+const path = require('path')
+const bodyParser = require('body-parser')
+
+
+const config = {
+    user: 'gainn',
+    password: 'ashutosh@123',
+    server: 'GAINN-PC22', // e.g. 'localhost'
+    database: 'HOLIDAY_MASTER_CSV',
+    options: {
+        encrypt: true, // For secure connection
+        trustServerCertificate: true,
+    }
+};
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/upload', upload.single('fileToUpload'), function (req, res, next) {
+    // parse the CSV file and insert the data into the database
+    const results = [];
+    fs.createReadStream(req.file.path)
+        .pipe(csvParser())
+        .on('data', (data) => {
+            results.push(data);
+        })
+        .on('end', () => {
+            sql.connect(config, function (err) {
+                if (err) console.log(err);
+
+                const request = new sql.Request();
+                let query = '';
+                for (let i = 0; i < results.length; i++) {
+                    const row = results[i];
+                    query += `INSERT INTO HOLIDAY (DATE, DAY, HOLIDAY) VALUES ('${row.Date}', '${row.Day}', '${row.Holiday}')\n`;
+                }
+                request.query(query, function (err, result) {
+                    if (err) console.log(err);
+                    console.log(`${results.length} records inserted into database.`);
+                    sql.close();
+                });
+            });
+            res.send('CSV file uploaded and records inserted into database successfully!');
+        });
+});
+
+
+app.listen(1212, () => console.log('app is running on 1212'));
